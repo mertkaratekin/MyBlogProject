be4b39b516b9e23b5c43a8ef08918b0aad66bd14
Mert Karatekin <mert1616karatekin@gmail.com>
2023-08-04 15:03:45 +0300

    EntityBase oluşturdum

---
+++ BlogProject.Core/Entities/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Core.Entities
+{
+    public class EntityBase : IEntityBase
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string CreatedBy { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual string? ModifiedBy { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
+        public virtual string? DeletedBy { get; set; }
+        public virtual DateTime? DeletedDate { get; set; }
+        public virtual bool Deleted { get; set; } = false;
+    }
+}
