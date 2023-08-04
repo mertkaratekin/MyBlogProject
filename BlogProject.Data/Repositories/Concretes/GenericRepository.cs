e86db3adde1e8f36b18bf715bd9daf6cd0078ae3
Mert Karatekin <mert1616karatekin@gmail.com>
2023-08-04 15:27:56 +0300

    Repository Design Pattern eklendi

---
+++ BlogProject.Data/Repositories/Concretes/GenericRepository.cs
@@ -0,0 +1,84 @@
+﻿using BlogProject.Data.Context;
+using BlogProject.Data.Repositories.Abstracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.Repositories.Concretes
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class, new()
+    {
+        private readonly AppDbContext _context;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private DbSet<T> Table
+        {
+            get => _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+        {
+            return Table.AnyAsync(predicate);
+        }
+
+        public Task<int> CountAsync(Expression<Func<T, bool>> predicate = null)
+        {
+            return Table.CountAsync(predicate);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            await Task.Run(() => Table.Remove(entity));
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = Table;
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            if (includeProperties.Any())
+                foreach (var item in includeProperties)
+                    query = query.Include(item);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = Table;
+            query = query.Where(predicate);
+
+            if (includeProperties.Any())
+                foreach (var item in includeProperties)
+                    query = query.Include(item);
+
+            return await query.SingleAsync();
+        }
+
+        public async Task<T> GetByGuidAsync(Guid id)
+        {
+            return await Table.FindAsync(id);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => Table.Update(entity));
+            return entity;
+        }
+
+    }
+}
