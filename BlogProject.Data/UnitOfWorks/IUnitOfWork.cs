5f710d8b56c41a459740398e4f282becd7a955bd
Mert Karatekin <mert1616karatekin@gmail.com>
2023-08-04 15:32:18 +0300

    UnitOfwork ekledim

---
+++ BlogProject.Data/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using BlogProject.Core.Entities;
+using BlogProject.Data.Repositories.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.UnitOfWorks
+{
+    public interface IUnitOfWork :IAsyncDisposable
+    {
+        IGenericRepository<T> GetRepository<T>() where T : class, IEntityBase, new();
+
+        Task<int> SaveAsync();
+        int Save();
+    }
+}
---
+++ BlogProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using BlogProject.Data.Context;
+using BlogProject.Data.Repositories.Abstracts;
+using BlogProject.Data.Repositories.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork /*Sürekli save changes yazmamak için*/
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        IGenericRepository<T> IUnitOfWork.GetRepository<T>()
+        {
+            return new GenericRepository<T>(_context);
+        }
+    }
+}
