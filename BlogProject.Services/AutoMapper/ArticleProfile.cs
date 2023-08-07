ee9a3e470a0e2b86f1ccb524e016152966ca9251
Mert <mertqkaratekin@gmail.com>
2023-08-07 17:26:48 +0300

    ArticleDto ve Category Dto oluşturuldu. Mapper sınıfıma eklemeleri yapıldı.

---
+++ BlogProject.Services/AutoMapper/ArticleProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BlogProject.Entity.DTOs.Articles;
+using BlogProject.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Services.AutoMapper
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<Article, ArticleDto>().ReverseMap();
+        }
+    }
+}
---
+++ BlogProject.Services/AutoMapper/CategoryProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BlogProject.Entity.DTOs.Categories;
+using BlogProject.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Services.AutoMapper
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
