b2589c470aceb58ba3aabc32af1738169ad5b7cf
Mert <mertqkaratekin@gmail.com>
2023-08-07 17:03:17 +0300

    Makale listelemek için controller oluşturuldu. Data table kullanabilmek için @section ve layotuna @rendersectionasync eklemesi yapıldı.

---
+++ BlogProject.Web/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Web.Areas.Admin.Controllers
+{
+    public class ArticleController : Controller
+    {
+        [Area("Admin")]
+        [Route("Admin/[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
