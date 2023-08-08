bd8bc88990716d8a086b92192e6ca57a64999f1d
Mert <mertqkaratekin@gmail.com>
2023-08-08 11:50:15 +0300

    CategoryController oluşturuldu. Index sayfası tasarlandı. CategoryDto'ya alanlar eklendi.

---
+++ BlogProject.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BlogProject.Services.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _categoryService.GetAllCategoriesNonDeletedAsync());
+        }
+    }
+}
