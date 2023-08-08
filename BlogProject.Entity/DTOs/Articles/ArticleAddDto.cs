600f226080e015249b35d4b27ffb5d132ce4e1bc
Mert <mertqkaratekin@gmail.com>
2023-08-08 08:33:14 +0300

    ArticleAddDto icindeki CategoryId propu geri eklendi.

--- BlogProject.Entity/DTOs/Articles/ArticleAddDto.cs
+++ BlogProject.Entity/DTOs/Articles/ArticleAddDto.cs
@@ -12,6 +12,7 @@ namespace BlogProject.Entity.DTOs.Articles
     {
         public string Title { get; set; }
         public string Content { get; set; }
+        public Guid CategoryId { get; set; }
         public IList<CategoryDto> Categories { get; set; }
         public Guid AppUserId { get; set; } = Guid.Parse("2C34DA79-F839-4AA8-95DE-1D31A3B39C28");
         public AppUser AppUser { get; set; }
