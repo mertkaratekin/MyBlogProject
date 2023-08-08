b72b388c82cb88593c2004fa48faac6af903c979
Mert <mertqkaratekin@gmail.com>
2023-08-08 13:40:15 +0300

    Userları Rolleri ile beraber Listeleme Yapıldı. Controller oluşturuldu. Mapleme yapıldı.

---
+++ BlogProject.Services/AutoMapper/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BlogProject.Entity.DTOs.Users;
+using BlogProject.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Services.AutoMapper
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserDto, AppUser>().ReverseMap();
+        }
+    }
+}
---
+++ BlogProject.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using BlogProject.Entity.DTOs.Users;
+using BlogProject.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProject.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var map = _mapper.Map<List<UserDto>>(users);
+
+            //Rolü bulabilmek için döngü ile tüm userları bulmamız lazım ki id sine göre rol bulayım.
+            foreach (var item in map)
+            {
+                var findUser = await _userManager.FindByIdAsync(item.Id.ToString());
+                var role = string.Join("~", await _userManager.GetRolesAsync(findUser)); //superadmin~admin~normaluser..... diye ayırıyor.
+
+                item.Role = role;
+            }
+
+            return View(map);
+        }
+    }
+}
