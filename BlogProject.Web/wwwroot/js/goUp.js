16b06c9afb7397b9973b6f9c89fe0d4b34d5e1a5
Mert <mertqkaratekin@gmail.com>
2023-08-09 13:33:56 +0300

    Yukarı Çık butonu eklendi. Layout düzenlendi. Sosyal medya linkleri eklendi.

---
+++ BlogProject.Web/wwwroot/js/goUp.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var goTopButton = document.getElementById("goTopButton");
+    window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 100) { /* Sayfanın 100 piksel altına inildiğinde butonu göster */
+            goTopButton.style.display = "block";
+        } else {
+            goTopButton.style.display = "none";
+        }
+    });
+
+    goTopButton.addEventListener("click", function () {
+        window.scrollTo(0, 0); /* Butona tıklandığında sayfanın başına kaydır */
+    });
+});
